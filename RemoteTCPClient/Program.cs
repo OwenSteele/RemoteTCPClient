164b614fd12dd7c1314aec71f778fb8eef9ea694
OwenSteele <69539828+OwenSteele@users.noreply.github.com>
2020-12-05 21:20:22 +0000

    Added SSL authentication.

--- RemoteTCPClient/Program.cs
+++ RemoteTCPClient/Program.cs
@@ -2,14 +2,22 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Collections;
+using System.Threading;
 
 namespace RemoteTCPClient
 {
-    class Program
+    sealed class Client
     {
         private static Socket _clientSocket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        private static Hashtable _certificateErrors = new();
+
         static void Main()
-        {
+        {    
             Console.Title = "OS CLIENT";
             LoopConnect();
             SendLoop();
@@ -17,30 +25,78 @@ namespace RemoteTCPClient
         }
         private static void SendLoop()
         {
-            while (true)
-            {
-                Console.Write("Enter server request: ");
-                byte[] sendBuffer = Encoding.ASCII.GetBytes(Console.ReadLine());
-                _clientSocket.Send(sendBuffer);
+            bool headers = true;
+            string functionTag = null;
 
-                byte[] receivedBuffer = new byte[1024];
-                int recieved = _clientSocket.Receive(receivedBuffer);
-                byte[] data = new byte[recieved];
-                Array.Copy(receivedBuffer, data, recieved);
-                Console.WriteLine("Received: " + Encoding.ASCII.GetString(data));
+            Console.WriteLine("\n        CONNECTED! \n\n       WAIT.");
+            Thread.Sleep(1000);
+            GetServerInfo();
+            try 
+            { 
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    if (headers) Console.Write("Enter server request: ");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    string msg;
+                    while (true)
+                    {
+                        msg = Console.ReadLine();
+                        if (!String.IsNullOrWhiteSpace(msg)) break;
+                    }
+                    byte[] sendBuffer = Encoding.ASCII.GetBytes(msg+functionTag);
+                    _clientSocket.Send(sendBuffer);
+
+                    byte[] receivedBuffer = new byte[2048];
+                    int recieved = _clientSocket.Receive(receivedBuffer);
+                    byte[] data = new byte[recieved];
+                    Array.Copy(receivedBuffer, data, recieved);
+                    string strData = Encoding.ASCII.GetString(data);
+                    if (strData.Contains("<NoH>"))
+                    {
+                        headers = false;
+                        strData = strData.Substring(5);
+                    }
+                    else headers = true;
+                    if (strData.Contains("<<") && strData.Contains(">>"))
+                    {
+                        functionTag = strData.Substring(strData.IndexOf("<<"), (strData.IndexOf(">>")-strData.IndexOf("<<"))+2);
+                        strData = strData.Substring(0,strData.IndexOf("<<"));
+                    }
+                    else functionTag = null;
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    if (headers) Console.Write("Received: ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine(strData);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
         private static void LoopConnect()
         {
             IPAddress serverIP;
             int serverPort = 0;
+            string serverName = null;
+            bool ssl = false;
             Console.Write("Enter Server External IP: ");
             
             while (true)
             {
                 string inputIP = Console.ReadLine();
                 if (IPAddress.TryParse(inputIP, out serverIP)) break;
-                else Console.Write("\r Invalid input, try again: ");
+                else Console.Write("\rInvalid input, try again: ");
             }
 
             Console.Write("Enter Server Port Number: ");
@@ -49,14 +105,40 @@ namespace RemoteTCPClient
                 string inputPort = Console.ReadLine();
                 if (Int32.TryParse(inputPort, out serverPort))
                 {
-                    if (serverPort < 0 || serverPort > 65535) Console.Write("\r Value out of legal port bound (0 to 65535), try again: ");
+                    if (serverPort < 0 || serverPort > 65535) Console.Write("\rValue out of legal port bound (0 to 65535), try again: ");
                     else break;
                 }
-                else Console.Write("\r Invalid input, try again: ");
+                else Console.Write("\rInvalid input, try again: ");
             }
+
+            Console.Write("SSL required? [Y/N]: ");
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+                if (key == ConsoleKey.Y)
+                {
+                    ssl = true;
+                    Console.Write("Enter Server name exactly: ");
+                    while (true)
+                    {
+                        string inputName = Console.ReadLine();
+                        if (!String.IsNullOrWhiteSpace(inputName))
+                        {
+                            serverName = inputName;
+                            break;
+                        }
+                        else Console.Write("\rInvalid input, try again: ");
+                    }
+                    break;
+                }
+                else if (key == ConsoleKey.N) break;
+                else Console.Write("\rInvalid input, try again [Y/N]: ");
+            }
+
             int attempts = 0;
             while (!_clientSocket.Connected)
             {
+                Console.WriteLine($"Attempting to connect");
                 try
                 {
                     attempts++;
@@ -68,7 +150,24 @@ namespace RemoteTCPClient
                     Console.Write($"\rConnection attempts : {attempts.ToString()}");
                 }
             }
-            Console.WriteLine("\nConnected.");
+            Console.WriteLine("\rConnected.");
+
+            if (ssl) Console.WriteLine($"\nServer SSL authentication " +
+                $"{(SSLCertification.HandShake(_clientSocket, serverName) ? "successful" : "failed")}.");
+        }
+        private static void GetServerInfo()
+        {
+            _clientSocket.Send(Encoding.ASCII.GetBytes("###`INITclientINFOrequest`###"));
+
+            byte[] receivedBuffer = new byte[1024];
+            int recieved = _clientSocket.Receive(receivedBuffer);
+            byte[] data = new byte[recieved];
+            Array.Copy(receivedBuffer, data, recieved);
+            Console.Clear();
+            Console.WriteLine("You are now connected to the server.\n");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Encoding.ASCII.GetString(data));
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
---
+++ RemoteTCPClient/SSLCertification.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace RemoteTCPClient
+{
+    public static class SSLCertification
+    {
+        public static bool HandShake(Socket clientSocket, string serverName)
+        {
+            TcpClient client = new();
+            client.Client = clientSocket;
+            SslStream sslStream = new(client.GetStream(), false,
+                new RemoteCertificateValidationCallback(SSLCertification.ValidateServerCertificate), null);
+
+            try { sslStream.AuthenticateAsClient(serverName); }
+            catch (AuthenticationException e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+                if (e.InnerException != null) Console.WriteLine($"Inner exception: {e.InnerException.Message}");
+                Console.WriteLine("Authentication failed - closing the connection.");
+                client.Close();
+
+                return false;
+            }
+
+            byte[] messageToServer = Encoding.ASCII.GetBytes("New client initial contact with server.");
+            sslStream.Write(messageToServer);
+            sslStream.Flush();
+
+            string messageFromServer = SSLCertification.ReadMessage(sslStream);
+            Console.WriteLine($"Message from server: {messageFromServer}");
+            client.Close();
+
+            return true;
+        }
+        private static string ReadMessage(SslStream sslStream)
+        {
+            byte[] buffer = new byte[2048];
+            StringBuilder messageData = new StringBuilder();
+            int bytes = -1;
+            do
+            {
+                bytes = sslStream.Read(buffer, 0, buffer.Length);
+
+                Decoder decoder = Encoding.UTF8.GetDecoder();
+                char[] chars = new char[decoder.GetCharCount(buffer, 0, bytes)];
+                decoder.GetChars(buffer, 0, bytes, chars, 0);
+                messageData.Append(chars);
+                // Check for EOF.
+                if (messageData.ToString().IndexOf("<EOF>") != -1) break;
+            } while (bytes != 0);
+
+            return messageData.ToString();
+        }
+        private static bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors)
+        {
+            if (sslPolicyErrors == SslPolicyErrors.None) return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);                    
+            return false; // Do not allow this client to communicate with unauthenticated servers.
+        }
+    }
+}
